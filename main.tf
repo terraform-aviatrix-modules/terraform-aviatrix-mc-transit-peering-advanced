ea52aa9de508fb2b4319d908fd71742a04d66df9
Dennizz <root@ipaddr.nl>
2021-08-05 16:17:26 +0200

    initial

---
+++ main.tf
@@ -0,0 +1,31 @@
+locals {
+  set1_data = [
+    for name, asn in var.set1 : {
+      set1_name = name
+      set1_as   = asn
+    }
+  ]
+
+  set2_data = [
+    for name, asn in var.set2 : {
+      set2_name = name
+      set2_as   = asn
+    }
+  ]
+
+  all_peerings = { for entry in setproduct(var.set1_data, var.set2_data) : "${entry[0].set1_name}_${entry[1].set2_name}" => merge(entry[0], entry[1]) }
+}
+
+resource "aviatrix_transit_gateway_peering" "peering" {
+  for_each                                    = local.all_peerings
+  transit_gateway_name1                       = each.value.c1_name
+  transit_gateway_name2                       = each.value.c2_name
+  prepend_as_path1                            = [each.value.c1_as]
+  prepend_as_path2                            = [each.value.c2_as]
+  enable_peering_over_private_network         = var.enable_peering_over_private_network
+  gateway1_excluded_network_cidrs             = var.excluded_cidrs
+  gateway2_excluded_network_cidrs             = var.excluded_cidrs
+  enable_single_tunnel_mode                   = var.enable_single_tunnel_mode
+  enable_insane_mode_encryption_over_internet = var.enable_insane_mode_encryption_over_internet
+  tunnel_count                                = var.tunnel_count
+}
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "set1" {
+  type        = map()
+  description = "Map object of transit gateway names and asn's to create full mesh peering from"
+}
+
+variable "set2" {
+  type        = map()
+  description = "Map object of transit gateway names and asn's to create full mesh peering from"
+}
+
+variable "enable_peering_over_private_network" {
+  type        = bool
+  description = "Enable to use a private circuit for setting up peering"
+  default     = false
+}
+
+variable "excluded_cidrs" {
+  type        = list(string)
+  description = "A list of CIDR's to exclude on the peers"
+  default     = []
+}
+
+variable "enable_single_tunnel_mode" {
+  type        = bool
+  description = "Enable single tunnel mode."
+  default     = false
+}
+
+variable "enable_insane_mode_encryption_over_internet" {
+  type        = bool
+  description = "Enable insane mode over internet"
+  default     = null
+}
+
+variable "tunnel_count" {
+  type        = number
+  description = "Amount of tunnels to build for insane mode over internet"
+  default     = null
+}
